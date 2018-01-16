123d4f44b3898285a606d841e2fcfee7322752ab
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-01-16 16:15:17 +0700

    build shared library

--- Makefile
+++ Makefile
@@ -2,18 +2,22 @@ BUILD_DIR=build
 
 HEADERS=pigpio/pigpio.h utility.h ringbuffer.h command.h generator.h axis.h device.h
 BIN=$(BUILD_DIR)/cnc
+LIB=$(BUILD_DIR)/cnc.so
 
-CF=-g -Wall -Ipigpio
+CF=-g -Wall -Ipigpio -fPIC
 LF=-lm -pthread -lpigpio -L$(BUILD_DIR)/pigpio/
 
 .PHONY: all clean dirs cnc pigpio
 
 all: dirs pigpio cnc
 
-cnc: $(BIN)
+cnc: $(BIN) $(LIB)
 
 $(BIN): $(BUILD_DIR)/main.o
 	gcc ${LF} $^ -o $@
+
+$(LIB): $(BUILD_DIR)/main.o
+	gcc ${LF} $^ -shared -o $@
 
 $(BUILD_DIR)/main.o: main.c $(HEADERS)
 	gcc ${CF} -c $< -o $@
--- main.c
+++ main.c
@@ -46,13 +46,60 @@ Cmd center_cmd(int ax, void *userdata) {
 	return cmd;
 }
 
-int main(int argc, char *argv[]) {
-	int i;
+
+Generator gen;
+Device dev;
+
+int cnc_init() {
 	printf("start\n");
 	if (gpioInitialise() < 0) {
 		printf("error gpio init\n");
 		return 1;
 	}
+
+	gen_init(&gen, 0x10);
+	dev_init(&dev, 2);
+
+	Axis *axis_x = &dev.axes[0];
+	Axis *axis_y = &dev.axes[1];
+	axis_init(axis_x,  6, 13, 19, 16);
+	axis_init(axis_y, 27, 22, 12, 26);
+
+	return 0;
+}
+
+int cnc_quit() {
+	Axis *axis_x = &dev.axes[0];
+	Axis *axis_y = &dev.axes[1];
+	axis_free(axis_x);
+	axis_free(axis_y);
+
+	dev_free(&dev);
+	gen_free(&gen);
+
+	gpioTerminate();
+	
+	printf("stop\n");
+
+	return 0;
+}
+
+int cnc_scan_x() {
+	Axis *axis_x = &dev.axes[0];
+	axis_scan(axis_x, &gen, SPEED, ACCEL);
+	printf("length: %d\n", axis_x->length);
+	return axis_x->length;
+}
+
+int cnc_scan_y() {
+	Axis *axis_y = &dev.axes[1];
+	axis_scan(axis_y, &gen, SPEED, ACCEL);
+	printf("length: %d\n", axis_y->length);
+	return axis_y->length;
+}
+
+int cnc_center() {
+	int i;
 
 	Cookie cookie;
 	for (i = 0; i < 2; ++i) {
@@ -61,38 +108,27 @@ int main(int argc, char *argv[]) {
 		cookie.chan[i].pos = 0;
 		cookie.chan[i].len = 0;
 	}
-	
-	Generator gen;
-	gen_init(&gen, 0x10);
-
-	Device dev;
-	dev_init(&dev, 2);
 
 	Axis *axis_x = &dev.axes[0];
 	Axis *axis_y = &dev.axes[1];
-	axis_init(axis_x,  5, 23, 12, 26);
-	axis_init(axis_y, 18, 17, 19, 16);
 	
-	axis_scan(axis_y, &gen, SPEED, ACCEL);
-	printf("length: %d\n", axis_y->length);
-	axis_scan(axis_x, &gen, SPEED, ACCEL);
-	printf("length: %d\n", axis_x->length);
 	cookie.chan[0].len = axis_x->length;
 	cookie.chan[1].len = axis_y->length;
 
 	dev_run(&dev, &gen, center_cmd, (void*) &cookie);
 	dev_clear(&dev);
 
-	// dev_run(&dev, &gen, get_cmd, (void*) &cookie);
+	return 0;
+}
 
-	axis_free(axis_x);
-	axis_free(axis_y);
-	dev_free(&dev);
-	gen_free(&gen);
+int main() {
+	cnc_init();
 
-	gpioTerminate();
-	
-	printf("stop\n");
+	cnc_scan_x();
+	//cnc_scan_y();
+	//cnc_center();
+
+	cnc_quit();
 
 	return 0;
 }
