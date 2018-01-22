279f3f19c264da4936eb7e34b5ed66578f600535
Alexey Gerasev <alexey.gerasev@gmail.com>
2018-01-22 18:48:46 +0700

    motion function

--- main.c
+++ main.c
@@ -11,36 +11,27 @@
 #include "axis.h"
 #include "device.h"
 
-#define SPEED 4000
-#define ACCEL 20000
-
 typedef struct {
-	int time;
-	int idx;
-	int pos;
-	int len;
+	int32_t pos;
+	int32_t vel;
+	int32_t acc;
 } Chan;
 
 typedef struct {
 	Chan chan[2];
 } Cookie;
 
-Cmd center_cmd(int ax, void *userdata) {
+Cmd move_cmd(int ax, void *userdata) {
 	Cookie *cookie = (Cookie*) userdata;
 	Cmd cmd = cmd_none();
-	if (ax == 0) {
-		if (cookie->chan[0].idx < 0) {
-			int center = cookie->chan[0].len/2;
-			cmd = cmd_move(center, SPEED, ACCEL);
-			cookie->chan[0].pos = center;
-			cookie->chan[0].idx = 0;
-		}
-	} else {
-		if (cookie->chan[1].idx < 0) {
-			int center = cookie->chan[1].len/2;
-			cmd = cmd_move(center, SPEED, ACCEL);
-			cookie->chan[1].pos = center;
-			cookie->chan[1].idx = 0;
+	if (ax == 0 || ax == 1) {
+		if (cookie->chan[ax].pos != 0) {
+			cmd = cmd_move(
+				cookie->chan[ax].pos,
+				cookie->chan[ax].vel,
+				cookie->chan[ax].acc
+			);
+			cookie->chan[ax].pos = 0;
 		}
 	}
 	return cmd;
@@ -86,49 +77,34 @@ int cnc_quit() {
 
 int cnc_scan_x() {
 	Axis *axis_x = &dev.axes[0];
-	axis_scan(axis_x, &gen, SPEED, ACCEL);
+	axis_scan(axis_x, &gen, 2000, 10000);
 	printf("length: %d\n", axis_x->length);
 	return axis_x->length;
 }
 
 int cnc_scan_y() {
 	Axis *axis_y = &dev.axes[1];
-	axis_scan(axis_y, &gen, SPEED, ACCEL);
+	axis_scan(axis_y, &gen, 2000, 10000);
 	printf("length: %d\n", axis_y->length);
 	return axis_y->length;
 }
 
-int cnc_center() {
-	int i;
-
+int cnc_move(int32_t px, int32_t py, int32_t vx, int32_t vy, int32_t ax, int32_t ay) {
 	Cookie cookie;
-	for (i = 0; i < 2; ++i) {
-		cookie.chan[i].time = 0;
-		cookie.chan[i].idx = -1;
-		cookie.chan[i].pos = 0;
-		cookie.chan[i].len = 0;
-	}
+	cookie.chan[0].pos = px;
+	cookie.chan[1].pos = py;
+	cookie.chan[0].vel = vx;
+	cookie.chan[1].vel = vy;
+	cookie.chan[0].acc = ax;
+	cookie.chan[1].acc = ay;
 
-	Axis *axis_x = &dev.axes[0];
-	Axis *axis_y = &dev.axes[1];
-	
-	cookie.chan[0].len = axis_x->length;
-	cookie.chan[1].len = axis_y->length;
-
-	dev_run(&dev, &gen, center_cmd, (void*) &cookie);
+	dev_run(&dev, &gen, move_cmd, (void*) &cookie);
+	gen_clear(&gen);
 	dev_clear(&dev);
 
 	return 0;
 }
 
 int main() {
-	cnc_init();
-
-	cnc_scan_x();
-	//cnc_scan_y();
-	//cnc_center();
-
-	cnc_quit();
-
 	return 0;
 }
