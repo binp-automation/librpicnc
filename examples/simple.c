e8bad6b6b7b0e59526586b2fe5623a33fafc0d74
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-07-04 17:37:16 +0700

    simple example

--- examples/simple.c
+++ examples/simple.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include <rpicnc.h>
 
 int main(int argc, char *argv[]) {
-	printf("Hello, RPiCNC!\n");
+	printf("start\n");
+
+	AxisInfo axis_info = {
+		.pin_step=12,
+		.pin_dir=16,
+		.pin_left=20,
+		.pin_right=21,
+
+		.position=1000000,
+		.length=2000000,
+	};
+	assert(cnc_init(1, &axis_info) == 0);
+
+	Cmd cmds[] = {cmd_move_vel(0, 100, 10000)};
+	Task task = {
+		.type=TASK_CMDS,
+		.cmds={
+			.cmds_count={sizeof(cmds)/sizeof(cmds[0])},
+			.cmds={cmds},
+		},
+	};
+	printf("%d\n", task.cmds.cmds_count[0]);
+	assert(cnc_run_task(&task) == 0);
+
 	return 0;
 }
