66ca8e78dec4133e963bac9d1101f7713d7764e8
Alexey Gerasev <alexey.gerasev@gmail.com>
2019-11-12 15:41:56 +0700

    update parameters

--- examples/turnover.c
+++ examples/turnover.c
@@ -23,24 +23,45 @@ int main(int argc, char *argv[]) {
 
 	assert(cnc_init(1, &axis_info) == 0);
 
-	int factor = 4;
-	float speed_vel = 500*factor;
-	uint32_t steps_vel = 1800*factor;
+	uint32_t delay = 1000*1000; // microseconds
+	int factor = 1; // step division factor
 
-	float speed_ini = 100*factor;
-	uint32_t steps_acc = 100*factor;
+	// continuous movement
+	float speed_vel = 1200*factor; // speed, Hz (steps per second)
+	uint32_t steps_vel = 200*30*factor; // number of steps
 
+	float speed_ini = 400*factor; // initial speed, Hz
+	uint32_t steps_acc = 20*30*factor; // number of steps during acceleration
+
+	// speed conversion from Hz to period in microseconds 
 	uint32_t period_vel = (uint32_t) (1e6/speed_vel);
 	uint32_t period_ini = (uint32_t) (1e6/speed_ini);
+	uint32_t period_mid = (uint32_t) (1e6/(0.5*speed_vel + 0.5*speed_ini));
 
 	Cmd cmds[] = {
+		// make some room for acceleration
+		cmd_move_acc(0, (steps_acc + 0)/2, period_ini, period_mid),
+		cmd_move_acc(0, (steps_acc + 1)/2, period_mid, period_ini),
+
+		cmd_wait(delay),
+
+		// accelerate and move forward
 		cmd_move_acc(1, steps_acc, period_ini, period_vel),
 		cmd_move_vel(1, steps_vel, period_vel),
 		cmd_move_acc(1, steps_acc, period_vel, period_ini),
-		cmd_wait(1000000),
+
+		cmd_wait(delay),
+		
+		// accelerate and move backward
 		cmd_move_acc(0, steps_acc, period_ini, period_vel),
 		cmd_move_vel(0, steps_vel, period_vel),
 		cmd_move_acc(0, steps_acc, period_vel, period_ini),
+
+		cmd_wait(delay),
+
+		// return to initial position
+		cmd_move_acc(1, (steps_acc + 1)/2, period_ini, period_mid),
+		cmd_move_acc(1, (steps_acc + 0)/2, period_mid, period_ini),
 	};
 
 	Task task = {
